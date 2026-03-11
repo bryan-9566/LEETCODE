4812e39735a481fb04f701cbec3cba9d7916cf38
BRYAN SELVAM V <bryan956627@gmail.com>
2026-03-11 11:03:30 +0530

    Time: 1 ms (97.90%) | Memory: 42.5 MB (79.19%) - LeetSync

---
+++ 258-add-digits/add-digits.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int addDigits(int num) {
+        while(num>=10)
+        {
+            int s=0;
+            while(num>0)
+            {
+                s=s+num%10;
+                num/=10;
+            }
+            num=s;
+        }
+        return num;
+        
+    }
+}
