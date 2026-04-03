f7e444dd55037481d3fe9708090bc459bb46997f
BRYAN SELVAM V <bryan956627@gmail.com>
2026-04-03 16:37:27 +0530

    Time: 0 ms (100.00%) | Memory: 47.5 MB (40.69%) - LeetSync

---
+++ 268-missing-number/missing-number.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+      int n=nums.length;
+      int xor1=0,xor2=0;
+      for(int i=0;i<n;i++)
+      {
+        xor2^=nums[i];
+        xor1^=i;
+      }  
+      xor1^=n;
+      return xor1^xor2;
+    }
+}
