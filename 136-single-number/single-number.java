e94de5b4454afafd1c822d92dcd58c10b07638c5
BRYAN SELVAM V <bryan956627@gmail.com>
2026-04-03 16:55:18 +0530

    Time: 1 ms (99.98%) | Memory: 46.9 MB (74.81%) - LeetSync

--- 136-single-number/single-number.java
+++ 136-single-number/single-number.java
@@ -1,21 +1,10 @@
 class Solution {
     public int singleNumber(int[] nums) {
-        for(int i=0;i<nums.length;i++)
+        int bry=0;
+        for(int n:nums)
         {
-            int count=0;
-            for(int j=0;j<nums.length;j++)
-            {
-                if(nums[i]==nums[j] && i!=j)
-                {
-                    count++;
-                }
-            }
-            if(count==0)
-            {
-                return nums[i];
-            }
+            bry^=n;
         }
-        return -1;
-        
+        return bry;
     }
-} 
+}
