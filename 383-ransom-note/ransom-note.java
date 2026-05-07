046e821d10b0c0151ff6de8c24e586d83990d5d8
BRYAN SELVAM V <bryan956627@gmail.com>
2026-05-07 21:36:50 +0530

    Time: 1 ms (99.90%) | Memory: 46.2 MB (80.08%) - LeetSync

--- 383-ransom-note/ransom-note.java
+++ 383-ransom-note/ransom-note.java
@@ -1,28 +1,19 @@
 class Solution {
     public boolean canConstruct(String ransomNote, String magazine) {
-        HashMap<Character,Integer> bry=new HashMap<>();
+        int[] hash=new int[26];
         for(int i=0;i<magazine.length();i++)
         {
             char ch=magazine.charAt(i);
-            bry.put(ch,bry.getOrDefault(ch,0)+1);
+            hash[ch-'a']++;
         }
         for(int i=0;i<ransomNote.length();i++)
         {
             char ch=ransomNote.charAt(i);
-            if(bry.containsKey(ch))
-            {
-                if(bry.get(ch)>0)
-                {
-                    int cnt=bry.get(ch);
-                    cnt--;
-                    bry.put(ch,cnt);
-                }
-                else
-                {
-                    return false;
-                }
-            }
-            else
+            hash[ch-'a']--;
+        }
+        for(int i=0;i<hash.length;i++)
+        {
+            if(hash[i]<0)
             {
                 return false;
             }
