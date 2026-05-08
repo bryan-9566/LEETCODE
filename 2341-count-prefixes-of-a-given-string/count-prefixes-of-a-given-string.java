87796d308c705eba57105046318fe2a2e2d7df60
BRYAN SELVAM V <bryan956627@gmail.com>
2026-05-08 11:20:36 +0530

    Time: 2 ms (14.46%) | Memory: 44.9 MB (42.86%) - LeetSync

--- 2341-count-prefixes-of-a-given-string/count-prefixes-of-a-given-string.java
+++ 2341-count-prefixes-of-a-given-string/count-prefixes-of-a-given-string.java
@@ -4,7 +4,20 @@ class Solution {
         for(int i=0;i<words.length;i++)
         {
             String word=words[i];
-            if(s.startsWith(word))
+            if(word.length()>s.length())
+            {
+                continue;
+            }
+            int bry=1;
+            for(int h=0;h<word.length();h++)
+            {
+                if(word.charAt(h)!=s.charAt(h))
+                {
+                    bry=0;
+                    break;
+                }
+            }
+            if(bry==1)
             {
                 cnt++;
             }
