3baa108b20561d2458ade2b60013540cc0d1776d
BRYAN SELVAM V <bryan956627@gmail.com>
2026-05-08 20:18:48 +0530

    Time: 2 ms (70.79%) | Memory: 55.5 MB (59.83%) - LeetSync

--- 169-majority-element/majority-element.java
+++ 169-majority-element/majority-element.java
@@ -1,34 +1,33 @@
 class Solution {
     public int majorityElement(int[] nums) {
-        int el=0;
-        int c=0;
+        int el=0,cnt=0;
         for(int i=0;i<nums.length;i++)
         {
-            if(c==0)
+            if(cnt==0)
             {
-                c=1;
                 el=nums[i];
+                cnt=1;
             }
             else if(nums[i]==el)
             {
-                c++;
+                cnt++;
             }
             else
             {
-                c--;
+                cnt--;
             }
         }
-        int bry=0;
+        cnt=0;
         for(int i=0;i<nums.length;i++)
         {
             if(nums[i]==el)
             {
-                bry++;
+                cnt++;
             }
         }
-        if(bry>(nums.length/2))
+        if(cnt>nums.length/2)
         {
-           return el;
+            return el;
         }
         return -1;
     }
