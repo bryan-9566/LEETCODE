4df0fcb3743f77e114e3974d2133f7dc0e08924d
BRYAN SELVAM V <bryan956627@gmail.com>
2026-05-12 12:53:29 +0530

    Time: 3 ms (75.07%) | Memory: 52.8 MB (21.41%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.java
+++ 229-majority-element-ii/majority-element-ii.java
@@ -1,54 +1,53 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
-        List<Integer> h=new ArrayList<>();
-        int el1=0,el2=0;
-        int c1=0,c2=0;
+        List<Integer> bry=new ArrayList<>();
+        int cnt1=0,cnt2=0,el1=0,el2=0;
         for(int i=0;i<nums.length;i++)
         {
-            if(nums[i]==el1)
+            if(cnt1==0 && nums[i]!=el2)
             {
-                c1++;
-            }
-            else if(nums[i]==el2)
-            {
-                c2++;
-            }
-            else if(c1==0)
-            {
-                c1=1;
+                cnt1=1;
                 el1=nums[i];
             }
-            else if(c2==0)
+            else if(cnt2==0 && nums[i]!=el1)
             {
-                c2=1;
+                cnt2=1;
                 el2=nums[i];
             }
-            else 
+            else if(nums[i]==el1)
             {
-                c1--;
-                c2--;
+                cnt1++;
+            }
+            else if(nums[i]==el2)
+            {
+                cnt2++;
+            }
+            else
+            {
+                cnt1--;
+                cnt2--;
             }
         }
-        int bry1=0,bry2=0;
+        cnt1=0;cnt2=0;
         for(int i=0;i<nums.length;i++)
         {
             if(nums[i]==el1)
             {
-                bry1++;
+                cnt1++;
             }
             else if(nums[i]==el2)
             {
-                bry2++;
+                cnt2++;
             }
         }
-        if(bry1>(nums.length/3))
+        if(cnt1>nums.length/3)
         {
-           h.add(el1);
+            bry.add(el1);
         }
-        if(bry2>nums.length/3 && el1!=el2)
+        if(cnt2>nums.length/3 && el2!=el1)
         {
-            h.add(el2);
+            bry.add(el2);
         }
-        return h;
+        return bry;
     }
 }
