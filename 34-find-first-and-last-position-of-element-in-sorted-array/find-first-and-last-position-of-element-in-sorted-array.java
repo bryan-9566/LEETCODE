7dcd0362aa5529d65100b3553cdaaecbb840d017
BRYAN SELVAM V <bryan956627@gmail.com>
2026-05-14 16:15:15 +0530

    Time: 0 ms (100.00%) | Memory: 48.2 MB (50.00%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
@@ -3,27 +3,22 @@ class Solution {
       int first=-1,last=-1;
       int n=nums.length;
       first=bfsearch(nums,n,target);
-      if(first==-1)
+      if(first==nums.length || nums[first]!=target)
       {
-        return new int[] {first,last};
-      }  
-      last=blsearch(nums,n,target);
-      return new int[] {first,last};
+        return new int[] {-1,-1};
+      }
+      return new int[] {first,blsearch(nums,n,target)-1};
     }
     public int bfsearch(int[] a,int n,int x)
     {
-        int first=-1;
+        int first=n;
         int low=0,high=n-1;
         while(low<=high)
         {
             int mid=low+(high-low)/2;
-            if(a[mid]==x)
+            if(a[mid]>=x)
             {
                 first=mid;
-                high=mid-1;
-            }
-            else if(a[mid]>x)
-            {
                 high=mid-1;
             }
             else
@@ -35,18 +30,14 @@ class Solution {
     }
     public int blsearch(int[] a,int n,int x)
     {
-        int last=-1;
+        int last=n;
         int low=0,high=n-1;
         while(low<=high)
         {
             int mid=low+(high-low)/2;
-            if(a[mid]==x)
+            if(a[mid]>x)
             {
                 last=mid;
-                low=mid+1;
-            }
-            else if(a[mid]>x)
-            {
                 high=mid-1;
             }
             else
