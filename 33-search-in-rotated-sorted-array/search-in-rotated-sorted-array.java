c5ee0e30d63bffc2c7838cdfebc946381178406d
BRYAN SELVAM V <bryan956627@gmail.com>
2026-05-15 09:40:29 +0530

    Time: 0 ms (100.00%) | Memory: 43.8 MB (45.29%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
@@ -1,8 +1,9 @@
 class Solution {
     public int search(int[] nums, int target) {
-      int low=0,high=nums.length-1;
-      while(low<=high)
-      {
+       int low=0,high=nums.length-1;
+       int ans=-1;
+       while(low<=high)
+       {
         int mid=low+(high-low)/2;
         if(nums[mid]==target)
         {
@@ -19,7 +20,7 @@ class Solution {
                 low=mid+1;
             }
         }
-        else
+        if(nums[mid]<=nums[high])
         {
             if(nums[mid]<=target && target<=nums[high])
             {
@@ -30,7 +31,7 @@ class Solution {
                 high=mid-1;
             }
         }
-      } 
-      return -1; 
+       } 
+       return ans;
     }
 }
