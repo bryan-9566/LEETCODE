dbef6fc2376c69e094d890dcae317217b62b99be
BRYAN SELVAM V <bryan956627@gmail.com>
2026-05-21 07:02:43 +0530

    Time: 0 ms (100.00%) | Memory: 42.8 MB (40.82%) - LeetSync

--- 812-rotate-string/rotate-string.java
+++ 812-rotate-string/rotate-string.java
@@ -1,32 +1,9 @@
 class Solution {
     public boolean rotateString(String s, String goal) {
-        if(s.length()!=goal.length())
+        if(s.length()==goal.length() && (s+s).contains(goal))
         {
-            return false;
-        }
-        for(int i=0;i<s.length();i++)
-        {
-            char[] bry=s.toCharArray();
-            reverse(bry,0,bry.length-1);
-            reverse(bry,0,i-1);
-            reverse(bry,i,bry.length-1);
-            if(new String(bry).equals(goal))
-            {
-                return true;
-            }
+            return true;
         }
         return false;
-
-    }
-    public void reverse(char[] nums,int i,int j)
-    {
-        while(i<j)
-        {
-            char temp=nums[i];
-            nums[i]=nums[j];
-            nums[j]=temp;
-            i++;
-            j--;
-        }
     }
 }
