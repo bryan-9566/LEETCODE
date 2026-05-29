3bb8d666247f3f18995da3e00c09d976d8ca604f
BRYAN SELVAM V <bryan956627@gmail.com>
2026-05-29 06:31:09 +0530

    Time: 0 ms (100.00%) | Memory: 44.2 MB (68.93%) - LeetSync

--- 2727-number-of-senior-citizens/number-of-senior-citizens.java
+++ 2727-number-of-senior-citizens/number-of-senior-citizens.java
@@ -4,12 +4,8 @@ class Solution {
         for(int i=0;i<details.length;i++)
         {
             String bry=details[i];
-            char ch=bry.charAt(11);
-            int age=(ch-'0')*10;
-            char ch1=bry.charAt(12);
-            int age1=ch1-'0';
-            int fage=age+age1;
-            if(fage>60)
+            int age=(bry.charAt(11)-'0')*10+bry.charAt(12)-'0';
+            if(age>60)
             {
                 cnt++;
             }
