f031455776170ffe2943ec0bf2cf903a182281c4
BRYAN SELVAM V <bryan956627@gmail.com>
2026-05-31 16:20:00 +0530

    Time: 1 ms (24.81%) | Memory: 42.8 MB (50.32%) - LeetSync

---
+++ 1737-maximum-nesting-depth-of-the-parentheses/maximum-nesting-depth-of-the-parentheses.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int maxDepth(String s) {
+        int max=Integer.MIN_VALUE,count=0;
+        for(int i=0;i<s.length();i++)
+        {
+            if(s.charAt(i)=='(')
+            {
+                count++;
+            }
+            else if(s.charAt(i)==')')
+            {
+                count--;
+            }
+            max=Math.max(max,count);
+        } 
+        return max;
+    }
+}
