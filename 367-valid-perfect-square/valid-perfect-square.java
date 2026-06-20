b192594f0c7a5380a378ec8f03c673028ab13562
BRYAN SELVAM V <bryan956627@gmail.com>
2026-06-20 05:59:58 +0530

    Time: 0 ms (100.00%) | Memory: 42.3 MB (18.58%) - LeetSync

---
+++ 367-valid-perfect-square/valid-perfect-square.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean isPerfectSquare(int num) {
+        int low=1,high=num;
+       while(low<=high)
+       {
+        int mid=low+(high-low)/2;
+        if((long)mid*mid==num)
+        {
+            return true;
+        }
+        else if((long)mid*mid<num)
+        {
+            low=mid+1;
+        }
+        else
+        {
+            high=mid-1;
+        }
+       } 
+       return false;
+    }
+}
