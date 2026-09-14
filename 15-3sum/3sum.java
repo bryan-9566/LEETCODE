f52a9643a28e2bf89d2edeceb67156d1246b6e4f
BRYAN SELVAM V <bryan956627@gmail.com>
2026-09-14 14:49:12 +0530

    Time: 33 ms (70.59%) | Memory: 59.4 MB (32.16%) - LeetSync

--- 15-3sum/3sum.java
+++ 15-3sum/3sum.java
@@ -1,36 +1,42 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-       Arrays.sort(nums);
-       List<List<Integer>> bry=new ArrayList<>();
-       for(int i=0;i<nums.length;i++)
-       {
-        if(i>0 && nums[i]==nums[i-1])
-         continue;
-        int j=i+1;
-        int k=nums.length-1;
-        while(j<k)
+        Arrays.sort(nums);
+        List<List<Integer>> bh=new ArrayList<>();
+        for(int i=0;i<nums.length;i++)
         {
-            int sum=nums[i]+nums[j]+nums[k];
-            if(sum<0)
+            if(i>0 && nums[i]==nums[i-1])
             {
-                j++;
+                continue;
             }
-            else if(sum>0)
+            int j=i+1;
+            int k=nums.length-1;
+            while(j<k)
             {
-                k--;
-            }
-            else
-            {
-                bry.add(Arrays.asList(nums[i],nums[j],nums[k]));
-                j++;
-                k--;
-                while(j<k && nums[j]==nums[j-1])
-                 j++;
-                while(j<k && nums[k]==nums[k+1])
-                 k--;
+                int sum=nums[i]+nums[j]+nums[k];
+                if(sum==0)
+                {
+                    bh.add(Arrays.asList(nums[i],nums[j],nums[k]));
+                    while(j<k && nums[j]==nums[j+1])
+                    {
+                        j++;
+                    }
+                    while(k>j && nums[k]==nums[k-1])
+                    {
+                        k--;
+                    }
+                    j++;
+                    k--;
+                }
+                else if(sum<0)
+                {
+                    j++;
+                }
+                else
+                {
+                    k--;
+                }
             }
         }
-       } 
-       return bry;
+        return bh;
     }
 }
