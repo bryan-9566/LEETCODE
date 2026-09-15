55dc13c7c3e029f92bcf2bf6805fe5b4ca426ffb
BRYAN SELVAM V <bryan956627@gmail.com>
2026-09-15 22:16:43 +0530

    Time: 18 ms (89.17%) | Memory: 45.7 MB (77.26%) - LeetSync

--- 18-4sum/4sum.java
+++ 18-4sum/4sum.java
@@ -1,41 +1,50 @@
 class Solution {
     public List<List<Integer>> fourSum(int[] nums, int target) {
-       List<List<Integer>> bry=new ArrayList<>();
-       Arrays.sort(nums);
-       for(int i=0;i<nums.length;i++)
-       {
-        if(i>0 && nums[i]==nums[i-1])
-         continue;
-        for(int j=i+1;j<nums.length;j++)
+        List<List<Integer>> bh=new ArrayList<>();
+        Arrays.sort(nums);
+        for(int i=0;i<nums.length;i++)
         {
-            if(j>i+1 && nums[j]==nums[j-1])
-             continue;
-            int k=j+1;
-            int l=nums.length-1;
-            while(k<l)
+            if(i>0 && nums[i]==nums[i-1])
             {
-                long sum=(long)nums[i]+nums[j]+nums[k]+nums[l];
+                continue;
+            }
+            for(int j=i+1;j<nums.length;j++)
+            {
+                if(j>i+1 && nums[j]==nums[j-1])
+                {
+                    continue;
+                }
+                int l=j+1;
+            int r=nums.length-1;
+            while(l<r)
+            {
+                long sum=(long)nums[i]+nums[j]+nums[l]+nums[r];
                 if(sum<target)
                 {
-                    k++;
+                    l++;
                 }
                 else if(sum>target)
                 {
-                    l--;
+                    r--;
                 }
                 else
                 {
-                    bry.add(Arrays.asList(nums[i],nums[j],nums[k],nums[l]));
-                    k++;
-                    l--;
-                    while(k<l && nums[k]==nums[k-1])
-                     k++;
-                    while(k<l && nums[l]==nums[l+1])
-                     l--;
+                    bh.add(Arrays.asList(nums[i],nums[j],nums[l],nums[r]));
+                    l++;
+                    r--;
+                    while(l<r && nums[l]==nums[l-1])
+                    {
+                        l++;
+                    }
+                    while(r>l && nums[r]==nums[r+1])
+                    {
+                        r--;
+                    }
                 }
             }
+            }
+            
         }
-       }
-       return bry;
+        return bh;
     }
 }
